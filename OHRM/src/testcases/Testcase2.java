f6fe80da9fc19c2319e029045de009e11eb5d679
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2022-03-30 20:32:12 +0530

    TestCase 2 Updated By Suresh

--- OHRM/src/testcases/Testcase2.java
+++ OHRM/src/testcases/Testcase2.java
@@ -4,9 +4,9 @@ public class Testcase2 {
 
 	public static void main(String[] args)
 	{
-		
-
+	
 		System.out.println("This is testcse 2");
+		System.out.println("TestCase 2 Modified....");
 	}
 
 }
