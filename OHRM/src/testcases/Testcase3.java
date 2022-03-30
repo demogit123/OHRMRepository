ba41458b84901d275882f5cd0d0a70b96d3f9728
sushm <sushm@LAPTOP-C2HKEGRB>
2022-03-30 20:40:24 +0530

    Test case 3 Added by Sushma

---
+++ OHRM/src/testcases/Testcase3.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class Testcase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Test case 3");
+
+	}
+
+}
